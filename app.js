c917fe3cb2bbd108e90606516f4e4ac11da782d3
Jdubstep1357 <jwlyons2015@gmail.com>
2023-02-19 14:55:12 -0600

    Implemented grid

--- app.js
+++ app.js
@@ -16,17 +16,29 @@ function flip() {
 // Creating Boards
 const width = 10
 
-function createBoard() {
+// whatever color is passed into createBoard() will be shown
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
-    gameBoardContainer.style.backgroundColor = 'pink'
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
 
-    // Takes takes created div in javascript into index.html
+    // Creates 100 blocks ie spaces for players to hit or miss
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        // Allows styling of .block in styles.css
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
+
+    // Takes takes above created div in javascript into index.html
     //gamesBoardContainer is referenced on line 1
     gamesBoardContainer.append(gameBoardContainer)
 }
 
-createBoard()
+createBoard('yellow', 'player')
+createBoard('blue', 'computer')
 
 // flip refers to function
 flipButton.addEventListener('click', flip)
