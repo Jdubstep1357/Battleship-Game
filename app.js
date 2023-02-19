dd98d4ef1601e4a19cd8832950efb6803c078d12
Jdubstep1357 <jwlyons2015@gmail.com>
2023-02-19 14:33:59 -0600

    More advanced flipping

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const flipButton = document.querySelector('#flip-button')
 let angle = 0
 function flip() {    
     const optionShips = Array.from(optionContainer.children)
-    if (angle == 0) {
-        angle = 90
-    } else {
-        angle = 0
-    }
+    // if (angle == 0) {
+    //     angle = 90
+    // } else {
+    //     angle = 0
+    // }
+
+    angle = angle === 0 ? 90: 0
     // CSS IN JAVASCRIPT ON CLICK
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
